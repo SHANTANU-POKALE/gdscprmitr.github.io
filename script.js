d1dff351935cf54e2e41fa79cd22da3ba673c232
sh@nt@nu <140294652+SHANTANU-POKALE@users.noreply.github.com>
2024-06-03 20:13:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,18 @@
-let menubtn = document.querySelectorAll('menu-Btn')
-let navbar = document .queryselector('.header .flex .navbar');
+/* script1.js */
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('#menu-bars');
+    const navbar = document.querySelector('.navbar');
 
-menubtn.onclick = () =>{
-	menuBtn.classlist.toggle('fa-times');
-	navbar.classlist.toggle('active');
-}
+    menu.onclick = () => {
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('active');
+    };
 
-   var swiper = new Swiper(".course-slider", {
-   	spaceBetween: 20,
-   	grabCursor:true,
-   	loop:true,
-      pagination: {
-        el: ".swiper-pagination",
-         clickable: true,
-      },
-      breakpoints: {
-        540: {
-          slidesPerView: 1,
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
         },
-        768: {
-          slidesPerView: 2, 
-        },
-        1024: {
-          slidesPerView: 3,          
-        },
-    },
+    });
 });
-
-
-   var swiper = new Swiper(".member-slider", {
-   	spaceBetween: 20,
-   	grabCursor:true,
-   	loop:true,
-      pagination: {
-        el: ".swiper-pagination",
-         clickable: true,
-      },
-      breakpoints: {
-        540: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2, 
-        },
-        1024: {
-          slidesPerView: 3,          
-        },
-    },
-});
-
-
-   var swiper = new Swiper(".reviews-slider", {
-   	spaceBetween: 20,
-   	grabCursor:true,
-   	loop:true,
-      pagination: {
-        el: ".swiper-pagination",
-         clickable: true,
-      },
-      breakpoints: {
-        540: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2, 
-        },
-        1024: {
-          slidesPerView: 3,          
-        },
-    },
-});
-
